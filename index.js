c6915e518e8e1edc6b5263bcb2112a7e7ad5fc5d
Kevin Zhou <zza203@sfu.ca>
2020-09-26 11:29:38 -0700

    formated index.js page

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const { resourceUsage } = require('process');
 const {Pool} = require('pg');
+const session = require('express-session');
 
 var pool = new Pool({
   connectionString :  process.env.DATABASE_URL
 })
 
+var app = express();
+var http = require('http').createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'scripts')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: false,
+  resave: false
+}))
+
+//section for all get post routes
+app.get('/',(req,res) =>  {
+  res.render('pages/index',{data:"hi"})
+})
 
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => {
-    var query = `SELECT * from ripple.user`
-    pool.query(query,(error,resp)=>{
-      if(error){
-        console.log(error);
-        res.send("error")
-      }
+http.listen(PORT,() => console.log(`Listening on ${ PORT }`));
 
-      res.render('pages/index',{data:resp.rows})
-    })
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
