7879350e956fe99c7b616dff101d0f97af45886d
Kevin Zhou <zza203@sfu.ca>
2020-09-27 04:21:26 -0700

    completed friends

--- index.js
+++ index.js
@@ -63,9 +63,154 @@ app.post('/login',(req,res) => {
   })
 })
 
+//post route for sending messages to the database
+app.post('/sendMessage',(req,res) => {
+  var username = req.session.user.username;
+  var friendName = req.body.friendUsername;
+  var content = req.body.message;
+
+  var sendMessageQUery = 'INSERT INTO ripple.message (userid1,userid2,since,msg,checked) VALUES ($1,$2,NOW()::TIMESTAMP,$3,$4)';
+  var sendRecentMessageQuery = 'INSERT INTO ripple.recentmsg (content,since,userid1,userid2) VALUES ($1,NOW()::TIMESTAMP,$2,$3) ON CONFLICT (userid1,userid2) DO UPDATE SET content = $4';
+
+  pool.query(sendMessageQUery,[username,friendName,content,false],(error,results) => {
+    if(error){
+      console.log(error)
+      console.log("send msg error!");
+      res.status(401);
+    }
+
+    pool.query(sendRecentMessageQuery,[content,username,friendName,content],(error,results2) => {
+      if(error){
+        console.log("send recentmsg error!");
+        res.status(401);
+      }
+
+      pool.query(sendRecentMessageQuery,[content,friendName,username,content],(error,results3) => {
+        if(error){
+          console.log("send recentmsg error!");
+          res.status(401);
+        }
+        
+        res.send({message:"sucess"});
+        
+      })
+    })
+  })
+})
+
+app.get('/getMessage',(req,res) => {
+  var username = req.session.user.username;
+  var friendname = req.query.friendname;
+
+  console.log(friendname);
+
+  var getMessageQuery = 'SELECT * from ripple.message where (userid1 = $1 and userid2 = $2) or (userid2 = $1 and userid1 = $2) order BY Since DESC'
+
+  pool.query(getMessageQuery,[username,friendname],(error,results) => {
+    if(error){
+      console.log("get friend msg error!");
+      res.status(401);
+    }
+
+    var messagedata =[];
+
+    results.rows.forEach(element => {
+      if(element.userid1 == username){
+        messagedata.push({
+          content:element.msg,
+          time: element.since,
+          owner: true
+        })
+      }
+      else{
+        messagedata.push({
+          content:element.msg,
+          time: element.since,
+          owner: false
+        })
+      }
+    })
+
+    res.send({messageData:messagedata})
+  })
+})
+
 //get route for the friends page
 app.get('/friends',(req,res) => {
-  res.render('pages/friends');
+  // console.log("in route")
+  var username = req.session.user.username;
+  var getFriends = 'SELECT * from ripple.friend where userid1 = $1 or userid2 = $1 ';
+  pool.query(getFriends,[username],(error,results) => {
+    if(error){
+      console.log("get friends error!");
+      res.status(401);
+    }
+
+    var realFriends = [];
+    var friendRequests = [];
+    var sentRequests = [];
+
+    results.rows.forEach(element => {
+      if(element.accepted == true){
+        if(element.userid1 == username){
+          var message = 
+          realFriends.push({name:element.userid2});
+          
+        }
+        else{
+          realFriends.push({name:element.userid1});
+        }
+      }
+      else{
+        if(element.userid1 == username){
+          sentRequests.push(element.userid2);
+        }
+        else{
+          friendRequests.push(element.userid1);
+        }
+      }
+    })
+
+    // console.log(realFriends);
+    // console.log(friendRequests);
+    // console.log(sentRequests);
+
+    var getRecentMsg = "Select * from ripple.recentmsg where userid1 = $1"
+    pool.query(getRecentMsg,[username],(error,results2) => {
+      if(error){
+        console.log("get latest msg error");
+        res.status(401);
+      }
+
+      if(results2.rows.length == 0){
+        realFriends.forEach(element=>{
+          element.message = '';
+        })
+      } 
+      else{
+        var date = {}
+        var messages ={}
+        results2.rows.forEach(ele => {
+          messages[ele.userid2] = ele.content;
+          date[ele.userid2] = ele.since;
+        })
+        realFriends.forEach(elem => {
+          elem.message = messages[elem.name];
+          elem.date = date[elem.name];
+        })
+      }
+      res.render('pages/friends',{
+        real: realFriends,
+        requests: friendRequests,
+        sent: sentRequests
+      });
+  
+
+    })
+  })
+
+  
+
 })
 
 http.listen(PORT,() => console.log(`Listening on ${ PORT }`));
