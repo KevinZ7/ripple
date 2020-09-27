385a5b04d5b9bbcbf241bf02bea7e0a994b61693
TingTing Li <tingtingli0325@gmail.com>
2020-09-27 12:33:50 -0700

    nav bar

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,40 +1,44 @@
 create schema if not exists ripple;
+drop table if exists ripple.friend;
+drop table if exists ripple.message;
+drop table if exists ripple.journal;
+drop table if exists ripple.quote;
+drop table if exists ripple.description;
+drop table if exists ripple.user;
 create table if not exists ripple.user (
     userid text primary key,
-    age int
+    age int not null
 );
 create table if not exists ripple.description (
     descid SERIAL primary key,
-    content text,
-    descencoding text,
-    dt timestamp,
+    content text not null,
+    since TIMESTAMP NOT null,
     userid text references ripple.user(userid) on delete cascade
 );
-
 create table if not exists ripple.quote (
-    quoteid text primary key,
-    quote text,
+    quoteid SERIAL primary key,
+    quote text not null,
+    author text,
+    since TIMESTAMP NOT null,
     userid text references ripple.user(userid) on delete cascade
 );
-
 create table if not exists ripple.journal(
-    journalid text primary key,
-    journal text,
-    dt timestamp,
+    journalid SERIAL primary key,
+    journal text not null,
+    since TIMESTAMP NOT null,
+    category text NOT null,
     userid text references ripple.user(userid) on delete cascade 
 );
-
 create table if not exists ripple.message(
-    messageid text primary key,
+    messageid SERIAL primary key,
     userid1 text references ripple.user(userid) on delete cascade,
     userid2 text references ripple.user(userid) on delete cascade,
-    msg text
+    since TIMESTAMP NOT null,
+    msg text,
+    checked boolean
 );
-
 create table if not exists ripple.friend(
     userid1 text references ripple.user(userid) on delete cascade,
     userid2 text references ripple.user(userid) on delete cascade,
     accepted boolean
 );
-
-alter table if exists ripple.journal add category text;
